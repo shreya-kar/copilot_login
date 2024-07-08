b9592f4ce9114ec89ef79d46185495631c8dbcef
SKAR0605 <119133184+SKAR0605@users.noreply.github.com>
2024-07-08 22:38:18 +0530

    second commit

--- src/components/login.js
+++ src/components/login.js
@@ -3,8 +3,8 @@ import flogo from '../logo_without_bg.svg';
 
 const Login = () =>  {
     return(
-        <div className = "bg-white px-10 py-10 rounded-3xl border-2 border-black">
-            <div className = "visible md:invisible lg:invisible xl:invisible">
+        <div className = "bg-white h-screen px-10 py-20 border-2 border-white">
+            <div className = "visible sm: visible md:invisible lg:invisible xl:invisible">
                 <img src = {flogo} className = "h-10 w-10"/>
             </div>
             <h1 className = "text-2xl font-bold">Log in to your account</h1>
@@ -13,24 +13,31 @@ const Login = () =>  {
                 <div>
                     <label className = "text-base font-normal text-custom-aquamarine">Email</label>
                     <input
-                        className = "w-full border-2 border-black rounded-xl p-2 bg-transparent"
+                        className = "w-full border-2 border-gray-200 rounded-xl p-1 bg-transparent"
                         placeholder = "Enter your work email"
                     />
                 </div>
-                <div>
+                <div className = "mt-4">
                     <label className = "text-base font-normal text-custom-aquamarine">Password</label>
                     <input
-                        className = "w-full border-2 border-black rounded-xl p-2 bg-transparent"
+                        className = "w-full border-2 border-gray-200 rounded-xl p-1 bg-transparent"
                         placeholder = "Enter your password"
                         type = "password"
                     />
                 </div>
-                <div className = "mt-8 flex flex-col">
-                    <button className = "active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-custom-blue text-white text-lg font-bold">Sign In</button>
+                <div className = "mt-4 flex justify-between">
+                    <div>
+                        <input type = "checkbox"/>
+                        <label className = "font-normal text-sm text-black">Remember</label>
+                    </div>
+                    <button className = "font-normal text-sm text-custom-aquamarine">Forgot password</button>
                 </div>
-                <div className = "mt-8 flex justify-center items-center">
+                <div className = "mt-4 flex flex-col">
+                    <button className = "active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-2 rounded-xl bg-custom-blue text-white text-lg font-bold">Sign In</button>
+                </div>
+                <div className = "mt-4 flex justify-center items-center">
                     <p className = "font-normal text-sm text-custom-gray">Don't have an account?</p>
-                    <button className = "font-normal text-sm text-custom-light-blue">Contact OnFinance AI</button>
+                    <button className = "font-normal text-sm text-custom-light-blue"> Sign up</button>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
       colors: {'custom-aquamarine':'#457E89',
         'custom-blue':'#197C92',
         'custom-gray':'#475467',
@@ -12,6 +19,9 @@ module.exports = {
         'custom-light-gray':'#939393',
         'custom-dark-gray':'#1A2E28',
         'custom-neon-green':'#49FDA6',},
+      backgroundImage: {
+        'neoImg' : "url('./neo_gpt_bg.svg')",
+      },
     },
   },
   plugins: [],
